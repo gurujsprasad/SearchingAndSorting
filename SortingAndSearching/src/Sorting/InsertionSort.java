22276ad514ffd24e5c64ba216e6029c28066fec2
Guruprasad <gurujsprasad@gmail.com>
2016-10-31 14:54:34 -0700

    insertion sort completed

--- SortingAndSearching/src/Sorting/InsertionSort.java
+++ SortingAndSearching/src/Sorting/InsertionSort.java
@@ -5,16 +5,21 @@ public class InsertionSort {
 		if(elements.length == 1)
 			return elements;
 		else{
-			int key = elements[0];
 			for(int i = 1; i < elements.length; i++){
-				
+				int key = elements[i];
+				int j = i-1;
+				while(j>=0 && elements[j] > key){
+					elements[j+1] = elements[j];
+					j = j-1;
+				}
+				elements[j+1] = key;
 			}
 		}
 		return elements;
 	}
 	
 	public static void main(String args[]){
-		int elements [] = {3,4,1,2,6,3,-1,0,45,630};
+		int elements [] = {3,4,1,2,6,630,3,-1,0,45};
 		elements = insertionSort(elements);
 		for(int i =0; i<elements.length; i++){
 			System.out.println(elements[i]);
